b76e6573cbe9a334f2fa8b77568d00d9c74464bd
alexis coudeyras <acoudeyras@gmail.com>
2014-04-14 15:32:02 +0200

    Update designer.js

--- designer.js
+++ designer.js
@@ -1,18 +1,7 @@
 (function(CustomTextField) {
 
-//    /* Default width and height of your widget */
     CustomTextField.setWidth('130');
     CustomTextField.setHeight('22');
-
-    /* Define custom event for your widget */
-    CustomTextField.addEvent('click');
-
-//    /* Customize existing properties */
-//    CustomTextField.customizeProperty('test', {
-//        sourceTitle: 'Test Source',
-//        title: 'Test Static Value',
-//        description: 'Add a datasource to this property.'
-//    });
 
     /* Add a Label property */
     CustomTextField.addLabel({
@@ -20,21 +9,6 @@
         'position': 'left'
     });
 
-//    /* Set the Design and Styles panels */
-//    CustomTextField.setPanelStyle({
-//        'fClass': true,
-//        'text': true,
-//        'background': true,
-//        'border': true,
-//        'sizePosition': true,
-//        'label': true,
-//        'disabled': ['border-radius']
-//    });
-
-//    /* Override widget's initialization */
-//    CustomTextField.prototype.init = function() {
-//        this.node.innerHTML = "Widget Text"; /* Include text inside the widget */
-//    }
 
 });
 
